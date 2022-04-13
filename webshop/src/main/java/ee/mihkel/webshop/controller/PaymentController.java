0026757f61fbfedc98bb0b82ab30ceac14fb85e6
mihkelvaher <mihkel.vaher@industry62.com>
2022-04-13 13:07:48 +0300

    Service, @Value application.properties, @Autowired ja bean

--- webshop/src/main/java/ee/mihkel/webshop/controller/PaymentController.java
+++ webshop/src/main/java/ee/mihkel/webshop/controller/PaymentController.java
@@ -1,55 +1,33 @@
 package ee.mihkel.webshop.controller;
 
+import ee.mihkel.webshop.model.input.EveryPayResponse;
 import ee.mihkel.webshop.model.output.EveryPayData;
+import ee.mihkel.webshop.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.ZonedDateTime;
 import java.util.Date;
 
 @RestController
 public class PaymentController {
 
-    @PostMapping("payment")
-    public String getPaymentLink(@RequestBody int amount) {
+    @Autowired
+    PaymentService paymentService;
+    // nagu PaymentService paymentService = new PaymentService()
+    // aga üks mälukoht koguaeg
+    // selleks et @Autowired saaks panna peab olema @Component või laadset
 
-//        {
-//            "api_username": "92ddcfab96e34a5f",
-//                "account_name": "EUR3D1",
-//                "amount": 10.00,
-//                "order_reference": "asd125",
-//                "nonce": "a9b7f7e79aaasdsde01b9902",
-//                "timestamp": "2022-04-13T09:44:15+03:00",
-//                "customer_url": "https://www.postimees.ee"
-//        }
-
-        // KOGU SISU LÄHEB SERVICE CLASSI SISSE
-        EveryPayData everyPayData = new EveryPayData();
-        everyPayData.setApi_username("92ddcfab96e34a5f");
-        everyPayData.setAmount(99.00);
-        everyPayData.setOrder_reference("abs" + Math.random());
-        everyPayData.setNonce("ad" + Math.random() + new Date());
-        everyPayData.setTimestamp(new Date().toString());
-        System.out.println(new Date().toString()); // log4j2
-        everyPayData.setCustomer_url("https://www.delfi.ee"); // serverisse üles heroku --
-        // java ja front-end (Angular/React)
-
-        // urli tõstame application.properties sisse
-        String url = "https://igw-demo.every-pay.com/api/v4/payments/oneoff";
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Basic OTJkZGNmYWI5NmUzNGE1Zjo4Y2QxOWU5OWU5YzJjMjA4ZWU1NjNhYmY3ZDBlNGRhZA==");
-
-        HttpEntity<EveryPayData> httpEntity = new HttpEntity<>(everyPayData, headers);
-
-        // üks ja sama new koguaeg --> @Autowired
-        RestTemplate restTemplate = new RestTemplate(); // võimaldab teha HTTP päringuid
-//        restTemplate.exchange(url, HttpMethod.POST,httpEntity,EveryPayResponse.class);
-
-        return "";
+    @PostMapping("payment")  // localhost:8080/payment    Body    80   text
+    public String getPaymentLink(@RequestBody String amount) {
+        System.out.println(paymentService);
+        return paymentService.getPaymentLink(amount);
     }
 }
---
+++ webshop/src/main/java/ee/mihkel/webshop/service/PaymentService.java
@@ -0,0 +1,82 @@
+package ee.mihkel.webshop.service;
+
+import ee.mihkel.webshop.model.input.EveryPayResponse;
+import ee.mihkel.webshop.model.output.EveryPayData;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.ZonedDateTime;
+import java.util.Date;
+
+@Service
+@Log4j2
+public class PaymentService {
+
+    @Value("${everypay.baseurl}")
+    String everyPayBaseUrl;
+
+    @Value("${everypay.credentials}")
+    String credentials;
+
+    @Value("${everypay.username}")
+    String username;
+
+    @Value("${everypay.account}")
+    String account;
+
+    @Value("${everypay.customerurl}")
+    String customerUrl;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public String getPaymentLink(String amount) {
+        // aktiveerisin koodibloki
+        // ctrl + alt + m
+        EveryPayData everyPayData = buildEveryPayData(amount);
+
+        // urli tõstame application.properties sisse
+        String url = everyPayBaseUrl + "/payments/oneoff";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Basic " + credentials);
+
+        HttpEntity<EveryPayData> httpEntity = new HttpEntity<>(everyPayData, headers);
+
+        // üks ja sama new koguaeg --> @Autowired
+        //RestTemplate restTemplate = new RestTemplate(); // võimaldab teha HTTP päringuid
+        ResponseEntity<EveryPayResponse> response = restTemplate.exchange(url, HttpMethod.POST,httpEntity, EveryPayResponse.class);
+        if (response.getStatusCodeValue() == 201 && response.getBody() != null) {
+            return response.getBody().getPayment_link();
+        }
+
+        return "";
+    }
+
+    private EveryPayData buildEveryPayData(String amount) {
+        // castimine
+//        long longNumber = 10L;
+//        int intNumber = (int) longNumber;
+        EveryPayData everyPayData = new EveryPayData();
+        everyPayData.setApi_username(username);
+        everyPayData.setAccount_name(account);
+        everyPayData.setAmount(Integer.parseInt(amount)); // parsimine
+        everyPayData.setOrder_reference("abs" + Math.random());
+        everyPayData.setNonce("ad" + Math.random() + new Date());
+        everyPayData.setTimestamp(ZonedDateTime.now().toString());
+        System.out.println(new Date().toString()); // log4j2
+        log.info(new Date().toString());
+        log.error("ERROR!!");
+        log.debug("adasdasd");
+        everyPayData.setCustomer_url(customerUrl); // serverisse üles heroku --
+        // java ja front-end (Angular/React)
+        return everyPayData;
+    }
+}
